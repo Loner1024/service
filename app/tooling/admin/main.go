83df2b1f31104b9c7a7a095806d894fdfc9d68cf
Loner1024 <xth12138@gmail.com>
2021-10-14 18:46:52 +0800

    Token Generation

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -6,15 +6,75 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	"io"
 	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func main() {
-	err := genKey()
+	err := genToken()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func genToken() error {
+	name := "private.pem"
+	file, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+
+	privatePEM, err := io.ReadAll(io.LimitReader(file, 1024*1024))
+	if err != nil {
+		return fmt.Errorf("reading auth private key: %w", err)
+	}
+
+	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privatePEM)
+	if err != nil {
+		return fmt.Errorf("parsing auth private key: %w", err)
+	}
+	claims := struct {
+		jwt.RegisteredClaims
+		Roles []string
+	}{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "service project",
+			Subject:   "123456789",
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(8790 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		},
+		Roles: []string{"ADMIN"},
+	}
+
+	method := jwt.GetSigningMethod("RS256")
+	token := jwt.NewWithClaims(method, claims)
+	token.Header["kid"] = "54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"
+	str, err := token.SignedString(privateKey)
+	if err != nil {
+		return err
+	}
+	fmt.Println("======== TOKEN BEGIN ========")
+	fmt.Println(str)
+	fmt.Println("======== TOKEN END ========")
+	fmt.Print("\n")
+
+	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return fmt.Errorf("mashaling public key: %w", err)
+	}
+	publicBlock := pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+
+	if err := pem.Encode(os.Stdout, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %w", err)
+	}
+	return nil
 }
 
 func genKey() error {
